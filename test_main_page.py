95912e6fe4acd4da23054fce52932c0c7b7892d3
Alina Li <alinali87@gmail.com>
2023-12-02 15:09:49 +0400

    refactor test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,12 @@ from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    url = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
-    page = LoginPage(browser=browser, url=url)
+    url = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser=browser, url=url)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser=browser, url=browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
