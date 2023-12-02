b7bd9566ab63ba11ca096fbfa069922b475d32eb
Alina Li <alinali87@gmail.com>
2023-12-02 12:19:09 +0400

    add test_main_page

--- conftest.py
+++ conftest.py
@@ -1,12 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-# TODO: Добавьте туда файл conftest.py из предыдущего модуля. Убедитесь дополнительно, что там есть параметр для задания языка интерфейса, по умолчанию равный "en".
+def pytest_addoption(parser):
+    parser.addoption('--language',
+                     action='store',
+                     default='en',
+                     help='Choose a language for site interface')
+
+
 @pytest.fixture(scope='function')
-def browser():
-    print('\nstart browser for a test...')
-    browser = webdriver.Chrome()
+def browser(request):
+    language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print('\nstart chrome browser for a test...')
+    browser = webdriver.Chrome(options=options)
     yield browser
     print('\nquit browser...')
     browser.quit()
